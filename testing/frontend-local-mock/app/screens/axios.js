57f48653468cc1bcf8d052f5e998fcd2ca6cc4e2
JodyB <jarb88@hotmail.co.uk>
2022-03-22 09:58:19 +0000

    Updating with lastest and added testing

---
+++ testing/frontend-local-mock/app/screens/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// export async function LoginAttempt(data) {
+//     console.log(`In LoginAttempt: ${data}`);
+//     const res = await axios
+//         .post(`http://localhost:3000/auth/sign-in`, data)
+//         .catch(function (error) {
+//             console.log(error.toJSON());
+//         });
+//     return res;
+// }
